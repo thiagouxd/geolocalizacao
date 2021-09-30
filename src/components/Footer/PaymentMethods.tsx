b06b2514f6792ba39bb74023ff578d7dbf26c81d
thiagouxd <thiago.uxbr@gmail.com>
2021-09-30 16:02:39 -0300

    Finalização dos métodos de pagamento e redes sociais

--- src/components/Footer/PaymentMethods.tsx
+++ src/components/Footer/PaymentMethods.tsx
@@ -1,4 +1,12 @@
+import styled from "styled-components"
 import Title from "./Title"
+import { ReactComponent as Amex } from "../../images/pgto_Amex.svg"
+import { ReactComponent as Boleto } from "../../images/pgto_Boleto.svg"
+import { ReactComponent as Discover } from "../../images/pgto_Discover.svg"
+import { ReactComponent as Elo } from "../../images/pgto_Elo.svg"
+import { ReactComponent as Hipercard } from "../../images/pgto_Hipercard.svg"
+import { ReactComponent as Mastercard } from "../../images/pgto_Mastercard.svg"
+import { ReactComponent as Visa } from "../../images/pgto_Visa.svg"
 
 const PaymentMethods = () => {
   return (
@@ -6,8 +14,24 @@ const PaymentMethods = () => {
       <Title>
         Formas de pagamento
       </Title>
+
+      <Methods>
+        <Amex />
+        <Boleto />
+        <Discover />
+        <Elo />
+        <Hipercard />
+        <Mastercard />
+        <Visa />
+      </Methods>
     </>
   )
 }
+
+const Methods = styled.div`
+  max-width: 240px;
+  display: flex;
+  flex-wrap: wrap;
+`
 
 export default PaymentMethods
--- src/components/Footer/SocialMedias.tsx
+++ src/components/Footer/SocialMedias.tsx
@@ -1,4 +1,8 @@
 import Title from "./Title"
+import { ReactComponent as Facebook } from "../../images/logo_redes_sociais_facebook.svg"
+import { ReactComponent as Instagram } from "../../images/logo_redes_sociais_instagram.svg"
+import { ReactComponent as Youtube } from "../../images/logo_redes_sociais_youtube.svg"
+import styled from "styled-components"
 
 const SocialMedias = () => {
   return (
@@ -6,8 +10,26 @@ const SocialMedias = () => {
       <Title>
         Redes Sociais
       </Title>
+
+      <SocialMediasList>
+        <a href="https://facebook.com/nike" target="_blank" rel="noreferrer">
+          <Facebook />
+        </a>
+        <a href="https://instagram.com/nike" target="_blank" rel="noreferrer">
+          <Instagram />
+        </a>
+        <a href="https://yotube.com/nike" target="_blank" rel="noreferrer">
+          <Youtube />
+        </a>
+      </SocialMediasList>
     </>
   )
 }
+
+const SocialMediasList = styled.div`
+  a {
+    opacity: 0.7;
+  }
+`
 
 export default SocialMedias
