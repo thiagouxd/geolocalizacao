1947ba8f1553f5d94bc105834e52e85045a96259
thiagouxd <thiago.uxbr@gmail.com>
2021-10-07 22:32:40 -0300

    Retirando linhas em branco

--- webapp/src/utils/nearestStores.ts
+++ webapp/src/utils/nearestStores.ts
@@ -28,13 +28,10 @@ const convertDistanceToKm = (storeLat: number, clientPositionLat: number, storeL
   const lat2 = clientPositionLat * Math.PI / 180;
   const hypoLat = (clientPositionLat - storeLat) * Math.PI / 180;
   const hypoLng = (clientPositionLng - storeLng) * Math.PI / 180;
-
   const a = Math.sin(hypoLat / 2) * Math.sin(hypoLat / 2) +
     Math.cos(lat1) * Math.cos(lat2) *
     Math.sin(hypoLng / 2) * Math.sin(hypoLng / 2);
-
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
   const distance = earthRadius * c;
 
   return formaterDistance(distance);
