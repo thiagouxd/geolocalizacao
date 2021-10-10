1ce0fff92bfce3593f254b29484dc2889ffd78a1
thiagouxd <thiago.uxbr@gmail.com>
2021-10-09 21:19:44 -0300

    Inserindo try catch para quando a api não está em pé

--- webapp/src/api/getClientsPosition.tsx
+++ webapp/src/api/getClientsPosition.tsx
@@ -1,10 +1,14 @@
 async function getClientPosition(inputAddress: string) {
-  const data = await fetch(`http://localhost:8080/input?value=${inputAddress}`)
-  const res = await data.json()
-  if (res.status === "ZERO_RESULTS" || res.status === "INVALID_REQUEST") {
-    return
-  } else {
-    return await res.candidates[0].geometry.location
+  try {
+    const data = await fetch(`http://localhost:8080/input?value=${inputAddress}`)
+    const res = await data.json()
+    if (res.status === "ZERO_RESULTS" || res.status === "INVALID_REQUEST") {
+      return
+    } else {
+      return await res.candidates[0].geometry.location
+    }
+  } catch {
+    alert("Não foi possível buscar.")
   }
 }
 
