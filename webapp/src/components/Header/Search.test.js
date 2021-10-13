2d8c4f2cd01052b9417fe335a0d3b32f4c4167bd
thiagouxd <thiago.uxbr@gmail.com>
2021-10-12 22:26:02 -0300

    Enviando páginas acessíveis para main

--- webapp/src/components/Header/Search.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import Search from "./Search"
-
-test("O input de busca de ter search como type", () => {
-  render(<Search />)
-  const input = screen.getByPlaceholderText("Buscar")
-  expect(input).toHaveProperty("type", "search")
-});
